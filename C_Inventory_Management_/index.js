5031c2ac5bdc5dce7011b42850a84787456270dc
Upeksha Herath <upekshadilshan000@gmail.com>
2023-09-11 17:39:36 +0530

    diagrams

--- C_Inventory_Management_/index.js
+++ C_Inventory_Management_/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const product = require('./src/routes/product');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // middle wear
 app.use(express.json());
